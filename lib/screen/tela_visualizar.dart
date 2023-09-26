b6d94ddf598eab38d6c141ebe9c10830e1198a88
João Aguiar <106329078+jotaaguiar@users.noreply.github.com>
2023-09-26 10:32:43 -0300

    version03

--- lib/screen/tela_visualizar.dart
+++ lib/screen/tela_visualizar.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:order_master/providers/pedido_provider.dart';
 
 class TelaVisualizar extends StatelessWidget {
-  final List<String> pedidos;
-
-  TelaVisualizar({required this.pedidos});
-
   @override
   Widget build(BuildContext context) {
+    final pedidoProvider = Provider.of<PedidoProvider>(context);
+    final pedidos = pedidoProvider.pedidos;
+    final precoTotal = pedidoProvider.calcularPrecoTotal(); // Calcule o preço total
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Visualizar Pedidos'),
@@ -15,46 +16,45 @@ class TelaVisualizar extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
+            Container(
+              color: Colors.blue, // Cor de fundo azul para o título
               padding: EdgeInsets.all(16.0),
               child: Text(
                 'Pedidos:',
-                style: TextStyle(fontSize: 18),
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white, // Cor do texto branco para o título
+                ),
               ),
             ),
+            SizedBox(height: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: pedidos.map((pedido) {
+                return Container(
+                  color: Colors.white, // Cor de fundo branca para cada item de pedido
+                  margin: EdgeInsets.symmetric(vertical: 4.0),
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    pedido,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 16),
             Container(
+              color: Colors.blue, // Cor de fundo azul para o total
               padding: EdgeInsets.all(16.0),
-              child: Column(
-                children: pedidos.map((pedido) {
-                  return PedidoBox(pedido: pedido);
-                }).toList(),
+              child: Text(
+                'Preço Total: R\$ ${precoTotal.toStringAsFixed(2)}',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white, // Cor do texto branco para o total
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class PedidoBox extends StatelessWidget {
-  final String pedido;
-
-  PedidoBox({required this.pedido});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      margin: EdgeInsets.only(bottom: 16),
-      child: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              pedido,
-              style: TextStyle(fontSize: 16),
             ),
           ],
         ),
